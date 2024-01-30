275cbcfe13f39ea919dde19d1a5f825c27906707
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2024-01-30 13:58:58 +0530

    Login in Action

--- Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.AuthAPI.Controllers
@@ -7,15 +10,39 @@ namespace Mango.Services.AuthAPI.Controllers
 	[ApiController]
 	public class AuthAPIController : ControllerBase
 	{
-		[HttpPost("register")]
-		public async Task<IActionResult> Register()
+		private readonly IAuthService _authService;
+		protected ResponseDto _response;
+        public AuthAPIController(IAuthService authService)
+        {
+            _authService = authService;
+			_response = new();
+        }
+
+        [HttpPost("register")]
+		public async Task<IActionResult> Register([FromBody] RegistrationRequestDto model)
 		{
+			var errorMessage = await _authService.Register(model);
+			if(!string.IsNullOrEmpty(errorMessage))
+			{
+				_response.IsSuccess = false;
+				_response.Message = errorMessage;
+				return BadRequest(_response);
+			}
 			return Ok();
 		}
 		[HttpPost("login")]
-		public async Task<IActionResult> Login()
+		public async Task<IActionResult> Login([FromBody] LoginRequestDto model)
 		{
-			return Ok();
+			var loginResponse = await _authService.Login(model);
+			if(loginResponse.User == null)
+			{
+				_response.IsSuccess = false;
+				_response.Message = "Username or Password is incorrect";
+				return BadRequest(_response);
+			}
+
+			_response.Result = loginResponse;
+			return Ok(_response);
 		}
 	}
 }
