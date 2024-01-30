36aae8c782b098bf2427cb85dc36ad611867fab8
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2024-01-30 22:08:02 +0530

    Dropdown for Role

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
 ﻿using Mango.Web.Models.Dto;
 using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Mango.Web.Controllers
 {
@@ -20,6 +22,12 @@ namespace Mango.Web.Controllers
 		[HttpGet]
 		public IActionResult Register()
 		{
+			var roleList = new List<SelectListItem>()
+			{
+				new SelectListItem { Text = SD.RoleAdmin, Value= SD.RoleAdmin },
+				new SelectListItem { Text = SD.RoleCustomer, Value= SD.RoleCustomer }
+			};
+			ViewBag.RoleList = roleList;
 			return View();
 		}
 
