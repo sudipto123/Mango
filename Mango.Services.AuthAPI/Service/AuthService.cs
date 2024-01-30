dccde2ab0fe8c9b0862ef5189ef4c6324ccab883
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2024-01-30 14:52:35 +0530

    Token in Action

--- Mango.Services.AuthAPI/Service/AuthService.cs
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -12,12 +12,14 @@ namespace Mango.Services.AuthAPI.Service
 		private readonly AppDbContext _db;
 		private readonly UserManager<ApplicationUser> _userManager;
 		private readonly RoleManager<IdentityRole> _roleManager;
+		private readonly IJwtTokenGenerator _jwtTokenGenerator;	
 
-        public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IJwtTokenGenerator jwtTokenGenerator)
         {
 			_db = db;
 			_userManager = userManager;
 			_roleManager = roleManager;		
+			_jwtTokenGenerator = jwtTokenGenerator;
         }
 
         public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
@@ -33,7 +35,7 @@ namespace Mango.Services.AuthAPI.Service
 
 			//if the user is found, Generate the JWT Token
 
-
+			var token = _jwtTokenGenerator.GenerateToken(user);
 
 			UserDto userDto = new()
 			{
@@ -46,7 +48,7 @@ namespace Mango.Services.AuthAPI.Service
 			LoginResponseDto loginResponseDto = new LoginResponseDto()
 			{
 				User = userDto,
-				Token = ""
+				Token = token
 			};
 
 			return loginResponseDto;
